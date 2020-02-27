e2bcbc62336d0c47f6c2cb2dbdefd455021da2ac
Saurabh Bansal <bless2saurabh@gmail.com>
2020-02-27 17:25:45 +0530

    Possible conflicting change

--- HelloJava.java
+++ HelloJava.java
@@ -3,7 +3,8 @@ public class HelloJava {
 	public static void main(String[] args) {
 
 		System.out.println("Hello Java");
-		  
+
+		 System.out.println("This may be a conflicting change"); 
 		 System.out.println("Code changes for a new branch");
 	
 
